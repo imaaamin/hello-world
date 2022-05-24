c8a9c847860445c2f04c884d3f637321b3a4e78d
Islam Amin <imaaamin@uwaterloo.ca>
2022-05-24 17:46:25 +0300

    added genre controller

---
+++ src/main/java/hello/world/GenreRepository.java
@@ -0,0 +1,27 @@
+package hello.world;
+
+import javax.transaction.Transactional;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import hello.world.domain.Genre;
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.data.annotation.Id;
+import io.micronaut.data.exceptions.DataAccessException;
+import io.micronaut.data.jdbc.annotation.JdbcRepository;
+import io.micronaut.data.model.query.builder.sql.Dialect;
+import io.micronaut.data.repository.PageableRepository;
+
+@JdbcRepository(dialect = Dialect.MYSQL) 
+public interface GenreRepository extends PageableRepository<Genre, Long> { 
+
+    Genre save(@NonNull @NotBlank String name);
+
+    @Transactional
+    default Genre saveWithException(@NonNull @NotBlank String name) {
+        save(name);
+        throw new DataAccessException("test exception");
+    }
+
+    long update(@NonNull @NotNull @Id Long id, @NonNull @NotBlank String name);
+}
---
+++ src/main/java/hello/world/domain/Genre.java
@@ -0,0 +1,42 @@
+package hello.world.domain;
+
+import javax.validation.constraints.NotNull;
+
+import io.micronaut.data.annotation.GeneratedValue;
+import io.micronaut.data.annotation.Id;
+import io.micronaut.data.annotation.MappedEntity;
+
+@MappedEntity
+public class Genre {
+
+    @Id
+    @GeneratedValue(GeneratedValue.Type.AUTO)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Genre{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
